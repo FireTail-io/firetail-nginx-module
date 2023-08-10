8836b98a6908a98cbb1e5d2f93cf97dfda073602
theteacat <joshua@firetail.io>
2023-08-10 11:25:27 +0100

    Implement method for initialising instance of Firetail middleware

--- src/validator/main.go
+++ src/validator/main.go
@@ -3,8 +3,45 @@ package main
 import (
 	"C"
 	"log"
+	"net/http"
 	"unsafe"
+
+	firetail "github.com/FireTail-io/firetail-go-lib/middlewares/http"
 )
+
+// Will hold our Firetail middleware in Go memory
+var firetailMiddleware func(next http.Handler) http.Handler
+
+// Creates our middleware instance with the provided OpenAPI spec and Firetail API key
+//
+//export CreateMiddleware
+func CreateMiddleware(specLocationBytes unsafe.Pointer, specLocationLength C.int, apiTokenBytes unsafe.Pointer, apiTokenLength C.int) C.int {
+	log.Println("Loading OpenAPI spec in Go...")
+
+	specLocationSlice := C.GoBytes(specLocationBytes, specLocationLength)
+	specLocation := string(specLocationSlice)
+	log.Println("OpenAPI spec location:", specLocation)
+
+	apiTokenSlice := C.GoBytes(apiTokenBytes, apiTokenLength)
+	apiToken := string(apiTokenSlice)
+	log.Println("Received API token of length", len(apiToken))
+
+	var err error
+	firetailMiddleware, err = firetail.GetMiddleware(&firetail.Options{
+		OpenapiSpecPath:          specLocation,
+		LogsApiToken:             apiToken,
+		LogsApiUrl:               "",
+		DebugErrs:                true,
+		EnableRequestValidation:  true,
+		EnableResponseValidation: true,
+	})
+	if err != nil {
+		log.Println("Failed to initialise Firetail middleware, err:", err.Error())
+		return 0
+	}
+
+	return 1
+}
 
 //export ValidateRequestBody
 func ValidateRequestBody(bodyCharPtr unsafe.Pointer, bodyLength C.int) C.int {
