1d42c204b1bf9dc6ed6a19363e745febfdab1205
theteacat <joshua@firetail.io>
2024-10-15 16:48:44 +0100

    rename var

--- src/nginx_module/firetail_module.c
+++ src/nginx_module/firetail_module.c
@@ -247,11 +247,11 @@ char *FiretailAllowUndefinedRoutesDirectiveCallback(ngx_conf_t *configuration_ob
                                                     void *http_main_config) {
   // Find the firetail_api_key_field given the config pointer & offset in cmd
   char *firetail_config = http_main_config;
-  ngx_str_t *firetail_url_field = (ngx_str_t *)(firetail_config + command_definition->offset);
+  ngx_str_t *firetail_allow_undefined_routes_field = (ngx_str_t *)(firetail_config + command_definition->offset);
 
   // Get the string value from the configuraion object
   ngx_str_t *value = configuration_object->args->elts;
-  *firetail_url_field = value[1];
+  *firetail_allow_undefined_routes_field = value[1];
 
   return NGX_CONF_OK;
 }
